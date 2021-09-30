30b457dac2cb2f44d23abb05dbdc745135e5f377
vivekduttamishra <vivek@conceptarchitect.in>
2021-09-30 13:35:35 +0530

    day#9 update#1

--- tax/tax-calculator-factory.h
+++ tax/tax-calculator-factory.h
@@ -1,10 +1,28 @@
 #pragma once
 #include <iostream>
+#include <string>
+#include <sstream>
 #include <exception>
 using namespace std;
 #include "tax-calculator.h"
 
+class CalculatorNotFound :public exception{
 
+    int year;
+
+    public:
+    CalculatorNotFound(int year){
+        this->year=year;
+    }
+
+    const char * what() const throw(){
+        
+      return "Tax Calculator Not Found";
+    }
+
+    int getYear() const { return year; }
+
+};
 
 class TaxCalculatorFactory{
 
@@ -20,14 +38,47 @@ class SimpleTaxCalculatorFactory:public TaxCalculatorFactory{
 
         switch(year){
 
-            case 2019: return new TaxCalculator2019();
+            case 2019: 
+                //return new TaxCalculator2019();
+                return TaxCalculator2019::getInstance();
 
-            case 2020: return new TaxCalculator2020();
+            case 2020: 
+                //return new TaxCalculator2020();
+                return TaxCalculator2020::getInstance();
 
-            default: throw new exception("invalid year");
-
+            default: {
+                
+                throw  CalculatorNotFound(year);
+            }
         };
 
+    }       
+
+};
+
+
+class SmartTaxCalculatorFactory:public TaxCalculatorFactory{
+
+    public:
+
+    TaxCalculator * forYear(int year){
+
+        // switch(year){
+
+        //     case 2019: 
+        //         return new TaxCalculator2019();
+
+        //     case 2020: 
+        //         return new TaxCalculator2020();
+
+        //     default: {
+                
+        //         throw  CalculatorNotFound(year);
+        //     }
+        // };
+
+
+        throw  CalculatorNotFound(year);
     }       
 
 };
--- tax/tax-calculator.h
+++ tax/tax-calculator.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <iostream>
+using namespace std;
 
 class TaxCalculator{
 
@@ -8,7 +10,16 @@ class TaxCalculator{
 
 
 class TaxCalculator2019: public TaxCalculator{
+    static TaxCalculator2019 * instance;
+    TaxCalculator2019(){}
+
     public:
+    static TaxCalculator2019 * getInstance(){
+        if(instance==NULL)
+            instance=new TaxCalculator2019;
+        return instance;
+    }
+
     int calculateTax(FinancialInfo info){
         
         return (info.income-info.expense-info.investment)*(0.2 -  0.05*(info.gender==Gender::Female));
@@ -17,23 +28,33 @@ class TaxCalculator2019: public TaxCalculator{
 };
 
 class TaxCalculator2020: public TaxCalculator {
+    private:
+     TaxCalculator2020(){
+         cout<<"tax calcualator 2020 created\n";
+     }
     public:
     int calculateTax(FinancialInfo info){       
         return (info.income-(info.expense>50000?50000:info.expense)-(info.investment>100000?100000:info.investment))* 0.15;
     }
-};
 
-class TaxCalculator2021_V1:public  TaxCalculator2020 {
+    static TaxCalculator2020 * getInstance(){
+        static TaxCalculator2020 * instance=new TaxCalculator2020();
 
-    public:
-    int calculateTax(FinancialInfo info){       
-        int tax= TaxCalculator2020::calculateTax(info);
-        if(info.gender==Gender::Female)
-            tax=tax*0.95;
-
-        return tax;
+        return instance;
     }
 };
+
+// class TaxCalculator2021_V1:public  TaxCalculator2020 {
+
+//     public:
+//     int calculateTax(FinancialInfo info){       
+//         int tax= TaxCalculator2020::calculateTax(info);
+//         if(info.gender==Gender::Female)
+//             tax=tax*0.95;
+
+//         return tax;
+//     }
+// };
 
 
 class TaxCalculator2021:public  TaxCalculator {
